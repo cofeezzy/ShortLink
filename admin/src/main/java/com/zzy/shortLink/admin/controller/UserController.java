12d1b7cda31d65c640295d9bcaa8cc5193977166
coffeezzy <736091588@qq.com>
2024-02-26 01:30:42 +0800

    用户模块测试

--- admin/src/main/java/com/zzy/shortLink/admin/controller/UserController.java
+++ admin/src/main/java/com/zzy/shortLink/admin/controller/UserController.java
@@ -1,4 +1,20 @@
 package com.zzy.shortLink.admin.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户管理控制层
+ */
+@RestController
 public class UserController {
+    /**
+     *根据用户名获得用户信息
+     */
+    @GetMapping("/api/shortLink/v1/user/{username}")
+    public String getUserByUsername(@PathVariable("username") String username){
+        return "Hi " + username;
+    }
 }
