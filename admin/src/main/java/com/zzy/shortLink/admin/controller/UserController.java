7e2d0436ddb355fd66281a35051416bad6259713
cofeezzy <736091588@qq.com>
2024-03-02 20:52:05 +0800

    布隆过滤器的构造引用

--- admin/src/main/java/com/zzy/shortLink/admin/controller/UserController.java
+++ admin/src/main/java/com/zzy/shortLink/admin/controller/UserController.java
@@ -9,6 +9,7 @@ import com.zzy.shortLink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -42,5 +43,15 @@ public class UserController {
     @GetMapping("/api/shortLink/v1/actual/user/{username}")
     public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username){
         return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
+    }
+
+    /**
+     * 查询用户名是否存在
+     * @param username
+     * @return
+     */
+    @GetMapping("/api/shortLink/user/has-username")
+    public Result<Boolean> hasUsername(@RequestParam("username") String username){
+        return Results.success(userService.hasUsername(username));
     }
 }
--- admin/src/main/java/com/zzy/shortLink/admin/service/UserService.java
+++ admin/src/main/java/com/zzy/shortLink/admin/service/UserService.java
@@ -16,4 +16,11 @@ public interface UserService extends IService<UserDO> {
      */
     UserRespDTO getUserByUsername(String username);
 
+    /**
+     * 查询用户名是否存在
+     * @param username 用户名
+     * @return 用户名存在返回True，用户名不存在返回False
+     */
+    Boolean hasUsername(String username);
+
 }
