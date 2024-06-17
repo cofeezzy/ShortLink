cf019df06cacb16510845e34cadfe8aa2018e02a
cofeezzy <736091588@qq.com>
2024-06-17 16:42:25 +0800

    feature:初始化短链接分组代码

---
+++ admin/src/main/java/com/zzy/shortLink/admin/dao/mapper/GroupMapper.java
@@ -0,0 +1,11 @@
+package com.zzy.shortLink.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zzy.shortLink.admin.dao.entity.GroupDO;
+
+/**
+ * 短链接分组持久层
+ */
+public interface GroupMapper extends BaseMapper<GroupDO> {
+
+}
---
+++ admin/src/main/java/com/zzy/shortLink/admin/service/GroupService.java
@@ -0,0 +1,11 @@
+package com.zzy.shortLink.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zzy.shortLink.admin.dao.entity.GroupDO;
+
+/**
+ * 短链接分组接口层
+ */
+
+public interface GroupService extends IService<GroupDO> {
+}
