224adb2a253511f6f75648a10eb474bbb38ebabd
cofeezzy <736091588@qq.com>
2024-06-24 17:02:53 +0800

    refactor:短链接分库分表+登录Token拦截上下文改进

--- admin/src/main/java/com/zzy/shortLink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/zzy/shortLink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,12 +1,17 @@
 package com.zzy.shortLink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import com.zzy.shortLink.admin.common.convention.exception.ClientException;
+import com.zzy.shortLink.admin.common.enums.UserErrorCodeEnum;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -18,18 +23,28 @@ public class UserTransmitFilter implements Filter {
 
     private final StringRedisTemplate stringRedisTemplate;
 
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/shortLink/admin/v1/user/login",
+            "/api/shortLink/admin/v1/user/has-username"
+    );
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
-        if(!Objects.equals(requestURI, "/api/shortLink/v1/user/login")){
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-
-            Object userinfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-            if(userinfoJsonStr !=null){
-                UserInfoDTO userInfoDTO = JSON.parseObject(userinfoJsonStr.toString(), UserInfoDTO.class);
-                UserContext.setUser(userInfoDTO);
+        if(!IGNORE_URI.contains(requestURI)){
+            String method = httpServletRequest.getMethod();
+            if(!(Objects.equals(requestURI, "/api/shortLink/admin/v1/user") && Objects.equals(method, "POST"))){
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if(!StrUtil.isAllNotBlank(username, token)){
+                    throw new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL);
+                }
+                Object userinfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                if(userinfoJsonStr !=null){
+                    UserInfoDTO userInfoDTO = JSON.parseObject(userinfoJsonStr.toString(), UserInfoDTO.class);
+                    UserContext.setUser(userInfoDTO);
+                }
             }
         }
         try {
--- admin/src/main/java/com/zzy/shortLink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/zzy/shortLink/admin/common/enums/UserErrorCodeEnum.java
@@ -7,6 +7,9 @@ import com.zzy.shortLink.admin.common.convention.errorcode.IErrorCode;
  */
 public enum UserErrorCodeEnum implements IErrorCode {
 
+    //前后端联调约定：如果是这个状态码，跳转到用户登录页面
+    USER_TOKEN_FAIL("A000200", "用户Token验证失败"),
+
     USER_NULL("B002000", "用户不存在"),
 
     USER_NAME_EXIST("B002001","用户名已存在"),
