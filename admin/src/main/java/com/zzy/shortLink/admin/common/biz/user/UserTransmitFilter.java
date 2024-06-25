7e547c589e3c11b08c4a5257aabb55a558c1a0dc
cofeezzy <736091588@qq.com>
2024-06-25 16:27:03 +0800

    refactor:短链接用户登录上下文补充（增加忽略的三个URL，登录，判断可用，注册）以及暂缓网关模块的拦截处理（先用returnJson

--- admin/src/main/java/com/zzy/shortLink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/zzy/shortLink/admin/common/biz/user/UserTransmitFilter.java
@@ -4,13 +4,17 @@ import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import com.zzy.shortLink.admin.common.convention.exception.ClientException;
+import com.zzy.shortLink.admin.common.convention.result.Results;
 import com.zzy.shortLink.admin.common.enums.UserErrorCodeEnum;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Objects;
 
@@ -28,6 +32,7 @@ public class UserTransmitFilter implements Filter {
             "/api/shortLink/admin/v1/user/has-username"
     );
 
+    @SneakyThrows
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
@@ -38,19 +43,46 @@ public class UserTransmitFilter implements Filter {
                 String username = httpServletRequest.getHeader("username");
                 String token = httpServletRequest.getHeader("token");
                 if(!StrUtil.isAllNotBlank(username, token)){
-                    throw new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL);
+                    returnJson((HttpServletResponse)servletResponse, JSON.toJSONString(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
+                    return;
                 }
-                Object userinfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-                if(userinfoJsonStr !=null){
-                    UserInfoDTO userInfoDTO = JSON.parseObject(userinfoJsonStr.toString(), UserInfoDTO.class);
-                    UserContext.setUser(userInfoDTO);
+                Object userinfoJsonStr;
+                try{
+                    userinfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    if(userinfoJsonStr == null){
+                        throw new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL);
+                    }
+                }catch (Exception ex){
+                    returnJson((HttpServletResponse)servletResponse, JSON.toJSONString(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
+                    return;
                 }
+                UserInfoDTO userInfoDTO = JSON.parseObject(userinfoJsonStr.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
             }
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
             UserContext.removeUser();
+        }
+    }
+
+    /**
+     * 临时代码，返回客户端数据，因为全局异常拦截器无法拦截filter中的异常，此处先用这个，后续网关处理
+     */
+    private void returnJson(HttpServletResponse response, String json) throws Exception{
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try{
+            writer = response.getWriter();
+            writer.print(json);
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(writer != null){
+                writer.close();
+            }
         }
     }
 }
