eeb774a3935ba37153d96cc480a2cc09fc22f0fd
cofeezzy <736091588@qq.com>
2024-07-03 17:43:30 +0800

    refactor:分页查询短链接时拼接协议。

--- project/src/main/java/com/zzy/shortLink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/zzy/shortLink/project/service/impl/ShortLinkServiceImpl.java
@@ -152,7 +152,11 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 .eq(ShortLinkDO::getEnableStatus, 0)
                 .orderByDesc(ShortLinkDO::getCreateTime);
         IPage<ShortLinkDO> resultPage = baseMapper.selectPage(shortLinkPageReqDTO, queryWrapper);
-        return resultPage.convert(each ->BeanUtil.toBean(each, ShortLinkPageRespDTO.class));
+        return resultPage.convert(each ->{
+            ShortLinkPageRespDTO result = BeanUtil.toBean(each, ShortLinkPageRespDTO.class);
+            result.setDomain("http://" + result.getDomain());
+            return result;
+        });
     }
 
     @Override
