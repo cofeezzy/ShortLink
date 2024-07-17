87ca72544ec16b77823daf041f7ba480e3e1801a
cofeezzy <736091588@qq.com>
2024-07-17 18:25:46 +0800

    feature:统计访问地区

--- project/src/main/java/com/zzy/shortLink/project/dao/mapper/LinkLocaleStatsMapper.java
+++ project/src/main/java/com/zzy/shortLink/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -15,9 +15,9 @@ public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
      */
 
     @Insert("""   
-            INSERT INTO t_link_locale_stats (full_short_url, gid, date, cnt, country, province, adcode, create_time, update_time, del_flag )
+            INSERT INTO t_link_locale_stats (full_short_url, gid, date, cnt, country, province, city, adcode, create_time, update_time, del_flag )
                             VALUES( #{linkLocaleStats.fullShortUrl}, #{linkLocaleStats.gid}, #{linkLocaleStats.date}, 
-                            #{linkLocaleStats.cnt}, #{linkLocaleStats.country}, #{linkLocaleStats.province}, #{linkLocaleStats.adcode},
+                            #{linkLocaleStats.cnt}, #{linkLocaleStats.country}, #{linkLocaleStats.province}, #{linkLocaleStats.city}, #{linkLocaleStats.adcode},
                             NOW(), NOW(), 0 ) ON DUPLICATE KEY UPDATE
                             cnt = cnt + #{linkLocaleStats.cnt};
             """)
