c82160be8f583bf70876a9315bc58cce43eba04d
cofeezzy <736091588@qq.com>
2024-08-02 14:10:27 +0800

    optimize:保持代码规范将缓存key 移至常量类

--- project/src/main/java/com/zzy/shortLink/project/mq/producer/ShortLinkStatsSaveProducer.java
+++ project/src/main/java/com/zzy/shortLink/project/mq/producer/ShortLinkStatsSaveProducer.java
@@ -1,11 +1,12 @@
 package com.zzy.shortLink.project.mq.producer;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+
+import static com.zzy.shortLink.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_TOPIC_KEY;
 
 @Component
 @RequiredArgsConstructor
@@ -13,10 +14,7 @@ public class ShortLinkStatsSaveProducer {
 
     private final StringRedisTemplate stringRedisTemplate;
 
-    @Value("${spring.data.redis.channel-topic.short-link-stats}")
-    private String topic;
-
     public void send(Map<String, String> producerMap){
-        stringRedisTemplate.opsForStream().add(topic, producerMap);
+        stringRedisTemplate.opsForStream().add(SHORT_LINK_STATS_STREAM_TOPIC_KEY, producerMap);
     }
 }
